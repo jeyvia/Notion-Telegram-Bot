706baea7af3f2f8e497467a273ddef583f8c48e4
jeyvia <e0725215@u.nus.edu>
2023-06-16 01:09:44 +0800

    Starting the application

---
+++ bot.py
@@ -0,0 +1,120 @@
+import logging
+import os
+from dotenv import load_dotenv
+from telegram import ParseMode
+
+import notion
+
+from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, Filters
+
+
+PORT = int(os.environ.get('PORT', 5000))
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+load_dotenv()
+TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+
+# Global variables bc i am lazy
+CONNECT = 1
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def daily_update(update, context):
+    user_name = update.message.from_user.first_name.lower()
+    context.bot.delete_message(
+        chat_id=update.effective_chat.id,
+        message_id=update.message.message_id
+    )
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"Hello {user_name}! Could I have your Database ID please?"
+    )
+    return CONNECT
+
+
+def notion_connect(update, context):
+    databaseID = update.message.text
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"Thank you. Allow me to retrieve your database."
+    )
+    activities = notion.connectToNotion(databaseID)
+    if activities == "":
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="The Database ID you provided could not be found. Have you configured your database with us?"
+                 "If yes, try again. "
+        )
+        return CONNECT
+    else:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"Here are your activities for today:\n{activities}",
+            parse_mode=ParseMode.HTML
+        )
+        # end of conversation
+        return ConversationHandler.END
+
+
+def cancel(update, context):
+    context.bot.delete_message(
+        chat_id=update.effective_chat.id,
+        message_id=update.message.message_id
+    )
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Your request has been cancelled."
+    )
+
+    # end of conversation
+    return ConversationHandler.END
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+    # on different commands - answer in Telegram
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("goodmorning", daily_update)],
+        states={
+            CONNECT: [
+                CommandHandler('cancel', cancel),
+                MessageHandler(Filters.text, notion_connect)
+            ],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    dp.add_handler(conv_handler)
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://notion-telegrambot.herokuapp.com/' + TOKEN)
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ notion.py
@@ -0,0 +1,82 @@
+import os
+from dotenv import load_dotenv
+import requests
+from datetime import date
+from operator import itemgetter
+import html
+
+load_dotenv()
+NOTION_TOKEN = os.environ.get('NOTION_TOKEN')
+today = date.today()
+headers = {
+    "Authorization": "Bearer " + NOTION_TOKEN,
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28",
+}
+
+
+def parse_notion_object(obj):
+    event_name = obj['properties']['Name']['title'][0]['plain_text']
+    event_date = obj['properties']['Date']['date']['start']
+    time = obj['properties']['Time']['rich_text']
+    if time:
+        time = obj['properties']['Time']['rich_text'][0]['plain_text']
+    else:
+        time = ""
+    tags = []
+    all_tags = obj['properties']['Tags']['multi_select']
+    for tag in all_tags:
+        tags.append(tag['name'])
+    url = obj['url']
+
+    return event_name, event_date, time, tags, url
+
+
+def get_activities(NOTION_DATABASE_ID):
+    activities = []
+    url = f"https://api.notion.com/v1/databases/{NOTION_DATABASE_ID}/query"
+    payload = {"page_size": 50}
+    response = requests.post(url, json=payload, headers=headers)
+    data = response.json()
+    if data['object'] == 'error':
+        return activities
+    for result in data["results"]:
+        event_name, event_date, event_time, event_tags, event_url = parse_notion_object(result)
+        if event_date == "2023-06-16":
+            activity = {
+                'Name': event_name,
+                'Time': event_time,
+                'Tags': ', '.join(event_tags),
+                'URL': event_url
+            }
+            activities.append(activity)
+    return activities
+
+
+def sort_activities(activities):
+    sortedActivities = sorted(activities, key=itemgetter('Time'))
+    return sortedActivities
+
+
+def craft_msg(activities):
+    final_msg = ""
+    for activity in activities:
+        msg = ""
+        msg += f"<b>Activity name:</b> {activity['Name']}\n"
+        if activity['Time']:
+            msg += f"<b>Time:</b> {activity['Time']}\n"
+        msg += f"<b>Tags:</b> {activity['Tags']}\n"
+        link_url = activity['URL']
+        html_link = f'<a href="{html.escape(link_url)}">Click here to open it in Notion App</a>'
+        msg += html_link
+        final_msg += (msg + "\n\n")
+    return final_msg
+
+
+def connectToNotion(curr_date):
+    activities = get_activities(curr_date)
+    if activities:
+        activities = sort_activities(activities)
+    else:
+        return ""
+    return craft_msg(activities)
